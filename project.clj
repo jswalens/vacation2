ccbfa5afbe1a3c31258e69df8b2dc38d2cb34c1d
Janwillem Swalens <jswalens@vub.ac.be>
2017-04-20 13:11:26 +0200

    Add fork of Clojure with transactional actors.

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject vacation2 "0.1.0-SNAPSHOT"
   :description "Vacation2 benchmark, based on STAMP's vacation benchmark."
+  :resource-paths ["resources/clojure-1.8.0-transactional-actors-2.jar"]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.cli "0.3.5" :exclusions [org.clojure/clojure]]
                  ;[com.taoensso/timbre "4.1.4" :exclusions [org.clojure/clojure]]
