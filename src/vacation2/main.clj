14b4a8b4dc5568e2ee031153cfbfcf1bcec9d4b4
Janwillem Swalens <jswalens@vub.ac.be>
2017-04-20 13:11:39 +0200

    Start implementation of benchmark: create requested number of customers.

--- src/vacation2/main.clj
+++ src/vacation2/main.clj
@@ -14,7 +14,17 @@
   (send logger (fn [_] (apply println args))))
 
 (def cli-options
-  [["-d" "--debug" "Print debug information"]
+  [["-n" "--customers N" "Number of customers"
+    :default 100
+    :parse-fn #(Integer/parseInt %)]
+   ["-r" "--reservations N" "Number of reservations"
+    ; Corresponds to parameter -t in vacation.
+    :default 1000
+    :parse-fn #(Integer/parseInt %)]
+   ;["-w" "--workers N" "Number of workers"
+   ; :default 20
+   ; :parse-fn #(Integer/parseInt %)]
+   ["-d" "--debug" "Print debug information"]
    ["-h" "--help" "Print help information"]])
 
 (def usage
@@ -24,6 +34,14 @@
 Options:
 
 " (:summary (clojure.tools.cli/parse-opts nil cli-options))))
+
+(def customer-behavior
+  (behavior
+    []
+    []
+    (log "start reservation")
+    (dosync
+      (println "TODO"))))
 
 (defn -main [& args]
   "Main function. `args` should be a list of command line arguments."
@@ -40,5 +58,17 @@ Options:
       (def log log-debug))
     (when-not (or print-error? print-help?)
       (log "options: " options)
-      (println "TODO")))
+      (let [reservations-per-customer
+              (quot (:reservations options) (:customers options))
+            customers
+              (for [i (range (:customers options))]
+                (spawn customer-behavior))]
+        (when (not= (rem (:reservations options) (:customers options)) 0)
+          (println "WARNING: number of reservations is not divisible by number"
+            "of customers. Using" reservations-per-customer "reservations per"
+            "customer, so only" (* reservations-per-customer (count customers))
+            "in total."))
+        (doseq [c customers]
+          (dotimes [_i reservations-per-customer]
+            (send c))))))
   (shutdown-agents))
