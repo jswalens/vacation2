f951dd9488310bc397a8f39a7edd8fe832bcbe44
Janwillem Swalens <jswalens@vub.ac.be>
2017-07-28 14:38:19 +0200

    Add two variant project.clj files, one that uses the Clojure version with dependencies (normal) and one with delays.

---
+++ project-dependency.clj
@@ -0,0 +1,11 @@
+(defproject vacation2 "0.1.0-SNAPSHOT"
+  :description "Vacation2 benchmark, based on STAMP's vacation benchmark."
+  :resource-paths ["resources/clojure-1.8.0-transactional-actors-3.jar"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/tools.cli "0.3.5" :exclusions [org.clojure/clojure]]
+                 [crypto-password "0.2.0"]
+                 ;[com.taoensso/timbre "4.1.4" :exclusions [org.clojure/clojure]]
+                 ]
+  :main ^:skip-aot vacation2.main
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
