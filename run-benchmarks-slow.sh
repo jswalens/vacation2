9ce0743abd47dcf5cb47f49089d7b32f32c1a903
Janwillem Swalens <jswalens@vub.ac.be>
2017-08-04 16:37:49 +0200

    Variations of benchmark: vary # threads in multiples of 2, instead of a non-uniform distribution.

--- run-benchmarks-slow.sh
+++ run-benchmarks-slow.sh
@@ -33,16 +33,16 @@ for i in 1 2 3 4 5
 do
     # ORIGINAL VERSION
     version="orig"
-    for w in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 18 20 22 24 26 28 30 32 36 40 44 48 52 56 60 64
+    for w in 1 2 4 6 8 10 12 14 16 18 20 22 24 26 28 30 32 34 36 38 40 42 44 46 48 50 52 54 56 58 60 62 64
     do
         ./lein run -- -v $version -w $w $PARAMETERS > "$result_path/$version-w$w-i$i.txt"
     done
 
     # TXACT VERSION
     version="txact"
-    for w in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 18 20 22 24 26 28 30 32 36 40 44 48 52 56 60 64
+    for w in 1 2 4 6 8 10 12 14 16 18 20 22 24 26 28 30 32 34 36 38 40 42 44 46 48 50 52 54 56 58 60 62 64
     do
-        for s in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 18 20 22 24 26 28 30 32 36 40 44 48 52 56 60 64
+        for s in 1 2 4 6 8 10 12 14 16 18 20 22 24 26 28 30 32 34 36 38 40 42 44 46 48 50 52 54 56 58 60 62 64
         do
             ./lein run -- -v $version -w $w -s $s $PARAMETERS > "$result_path/$version-w$w-s$s-i$i.txt"
         done
