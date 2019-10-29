fe79e25c3a6b75a1db8ddaa541e3ef5540f0542c
Janwillem Swalens <jswalens@vub.be>
2019-10-29 15:31:13 +0100

    Put everything in one graph.

--- results/plot-speedup.py
+++ results/plot-speedup.py
@@ -5,6 +5,7 @@ from collections import defaultdict, OrderedDict
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+import matplotlib.colors as colors
 
 if len(sys.argv) >= 2:
     FILE = sys.argv[1]
@@ -14,10 +15,44 @@ else:
 if "-medians" in FILE:
     print("Warning: do not use -medians.csv, but original, so I can calculate error bars.")
 
-OUTPUT_ORIGINAL_NAME = re.sub(r"(-medians)?\.csv$", "-original-speedup-graph.pdf", FILE)
-OUTPUT_TXACT_NAME = re.sub(r"(-medians)?\.csv$", "-txact-speedup-graph.pdf", FILE)
+OUTPUT_NAME = re.sub(r"(-medians)?\.csv$", "-speedup.pdf", FILE)
+OUTPUT_ORIGINAL_NAME = re.sub(r"(-medians)?\.csv$", "-original-speedup.pdf", FILE)
+OUTPUT_TXACT_NAME = re.sub(r"(-medians)?\.csv$", "-txact-speedup.pdf", FILE)
 
 VERSIONS = ["original", "txact"]
+
+# (version, s)s to plot
+VSS = [ # ordered
+    ("original", 1),
+    ("txact", 1),
+    ("txact", 2),
+    ("txact", 8),
+    ("txact", 64),
+]
+
+LABELS = {
+    ("original", 1): "Original version",
+    ("txact", 1):    "Using tx actors, s = 1",
+    ("txact", 2):    "Using tx actors, s = 2",
+    ("txact", 8):    "Using tx actors, s = 8",
+    ("txact", 64):   "Using tx actors, s = 64",
+}
+
+# VUB orange = #FF6600 in HSV = 24,1.00,1.00
+palette = [
+    [ 0, 1.00, 0.25],
+    [ 8, 1.00, 0.50],
+    [16, 1.00, 0.75],
+    [24, 1.00, 1.00],
+]
+palette = [colors.hsv_to_rgb([c[0] / 360.0, c[1], c[2]]) for c in palette]
+COLORS = {
+    ("original", 1): "#003399",
+    ("txact", 1):     palette[0],
+    ("txact", 2):     palette[1],
+    ("txact", 8):     palette[2],
+    ("txact", 64):    palette[3],
+}
 
 def parse_file(filename):
     results = defaultdict(lambda: defaultdict(list))  # version -> (w, s) -> [time]
@@ -98,78 +133,20 @@ def calculate_errors(speedups):
         errors[w_s] = [error_first, error_third]
     return errors
 
-def draw_speedup_original(speedups, errors):
+def draw_speedup(speedups_original, errors_original, speedups_txact, errors_txact):
     # Type 1 fonts
     plt.rcParams["ps.useafm"] = True
     plt.rcParams["pdf.use14corefonts"] = True
-    #plt.rcParams["text.usetex"] = True
 
     sns.set_style("whitegrid", {"grid.color": ".9"})
 
-    x_values = [w for (w, s) in speedups.keys()]
-
-    plt.figure(figsize=(6, 2))
     ax = plt.axes()
     sns.despine(top=True, right=True, left=True, bottom=True)
 
-    #ax.set_title("Speed-up of original version", fontsize="x-large")
-
-    ax.set_xlabel(r"Number of worker actors ($p$)", fontsize="large")
-    #ax.set_xscale("log", basex=2)
-    xticks = [x for x in x_values if (x % 4 == 0) or (x == 1)]
-    ax.set_xticks(xticks)
-    ax.set_xticklabels(xticks)
-    ax.set_xlim(0.8, 64.2)
-
-    ax.set_ylabel("Speed-up", fontsize="large")
-    ax.set_ylim(0, 3.01)
-    #ax.set_yticks([0.0, 1.0, 2.0, 3.0])
-
-    median_speedups = [quarts["median"] for quarts in speedups.values()]
-    errors_ = np.transpose([es for es in errors.values()])
-    line = ax.errorbar(x=x_values, y=median_speedups,
-        yerr=errors_,
-        #color=COLORS[variation]
-        )
-
-    arrowprops = {
-        "arrowstyle": "->",
-        "color": "black",
-        "connectionstyle": "angle3,angleA=0,angleB=90",
-        "shrinkB": 5,
-    }
-    ax.annotate(xy=(1, 1), s="time = 5656 ms",
-        xytext=(30, -25), textcoords="offset points", arrowprops=arrowprops)
-    ax.annotate(xy=(42, 2.5), s="For $p = 42$:\nspeed-up = 2.5\ntime = 2230 ms",
-        xytext=(0, -60), textcoords="offset points", arrowprops=arrowprops)
-
-    plt.savefig(OUTPUT_ORIGINAL_NAME, bbox_inches="tight")
-    #plt.show()
-
-def draw_speedup_txact(speedups, errors):
-    # Type 1 fonts
-    plt.rcParams["ps.useafm"] = True
-    plt.rcParams["pdf.use14corefonts"] = True
-    #plt.rcParams["text.usetex"] = True
-
-    sns.set_style("whitegrid", {"grid.color": ".9"})
-
-    w_values = sorted(set(w for (w, s) in speedups.keys()))
-    #s_values = sorted(set(s for (w, s) in speedups.keys()))
-    s_values = [1, 2, 8, 64]
-
-    #sns.set_palette(sns.husl_palette(len(s_values), h=50/360, l=.75, s=1))
-    #sns.set_palette(sns.husl_palette(len(s_values), h=12/360, l=.5, s=1))
-    sns.set_palette(sns.cubehelix_palette(len(s_values), reverse=True))
-
-    plt.figure()
-    ax = plt.axes()
-    sns.despine(top=True, right=True, left=True, bottom=True)
-
-    #ax.set_title("Speed-up of original version", fontsize="x-large")
+    w_values = sorted(set(w for (w, s) in speedups_txact.keys()))
+    s_values = sorted(set(s for (v, s) in VSS))
 
     ax.set_xlabel(r"Number of primary worker actors ($p$)", fontsize="large")
-    #ax.set_xscale("log", basex=2)
     xticks = [x for x in w_values if (x % 4 == 0) or (x == 1)]
     ax.set_xticks(xticks)
     ax.set_xticklabels(xticks)
@@ -179,18 +156,20 @@ def draw_speedup_txact(speedups, errors):
     ax.set_ylim(0, 12.01)
 
     lines = {}
-    for s in s_values:
-        x = w_values
-        median_speedups = [speedups[(w, s)]["median"] for w in w_values]
-        errors_ = np.transpose([errors[(w, s)] for w in w_values])
-        line = ax.errorbar(x=x, y=median_speedups,
-            yerr=errors_,
-            #color=COLORS[s]
-            )
-        lines[s] = line
 
-    ax.legend([lines[s] for s in s_values], ["$s = %i$" % s for s in s_values],
-        loc="upper left", prop={"size": "small"})
+    medians_original = [quarts["median"] for quarts in speedups_original.values()]
+    errors_original = np.transpose([es for es in errors_original.values()])
+    lines[("original", 1)] = ax.errorbar(x=w_values, y=medians_original,
+        yerr=errors_original, color=COLORS[("original", 1)])
+
+    for s in s_values:
+        medians = [speedups_txact[(w, s)]["median"] for w in w_values]
+        errors = np.transpose([errors_txact[(w, s)] for w in w_values])
+        lines[("txact", s)] = ax.errorbar(x=w_values, y=medians,
+            yerr=errors, color=COLORS[("txact", s)])
+
+    ax.legend([lines[vs] for vs in VSS], [LABELS[vs] for vs in VSS],
+       loc="upper left", prop={"size": "small"})
 
     def arrowprops(n=90):
         return {
@@ -199,27 +178,30 @@ def draw_speedup_txact(speedups, errors):
             "connectionstyle": "angle3,angleA=0,angleB={}".format(n),
             "shrinkB": 5,
         }
-    ax.annotate(xy=(1, 0.4), s="For $p = 1$, $s = 1$:\nspeed-up = 0.4\ntime = 14377 ms",
-        xytext=(70, 10), textcoords="offset points", arrowprops=arrowprops(10))
-    ax.annotate(xy=(38, 11.0), s="For $p = 38$, $s = 8$:\nspeed-up = 11.0\ntime = 516 ms",
-        xytext=(60, -5), textcoords="offset points", arrowprops=arrowprops(90))
+
+    ax.annotate(xy=(1, 1), s="Original with $p = 1$:\ntime = 5656 ms",
+        xytext=(5, 130), textcoords="offset points", arrowprops=arrowprops())
+    ax.annotate(xy=(42, 2.5), s="Original with $p = 42$:\nspeed-up = 2.5\ntime = 2230 ms",
+        xytext=(0, -50), textcoords="offset points", arrowprops=arrowprops())
+
+    ax.annotate(xy=(1, 0.4), s="Tx act with $p = 1$, $s = 1$:\nspeed-up = 0.4\ntime = 14377 ms",
+        xytext=(50, -5), textcoords="offset points", arrowprops=arrowprops(10))
+    ax.annotate(xy=(38, 11.0), s="Tx act with $p = 38$, $s = 8$:\nspeed-up = 11.0\ntime = 516 ms",
+        xytext=(50, -5), textcoords="offset points", arrowprops=arrowprops(90))
     # Find best speed-up for s = 1:
     # print(sorted(
-    #         ((w, speedup["median"]) for ((w, s), speedup) in speedups.items() if s == 1),
+    #         ((w, speedup["median"]) for ((w, s), speedup) in speedups_txact.items() if s == 1),
     #         key=lambda w_res: w_res[1], reverse=True)[0])
-    ax.annotate(xy=(32, 5.9), s="For $p = 32$, $s = 1$:\nspeed-up = 6.1\ntime = 933 ms",
-        xytext=(0, -70), textcoords="offset points", arrowprops=arrowprops(90))
+    ax.annotate(xy=(32, 5.9), s="Tx act with $p = 32$, $s = 1$:\nspeed-up = 6.1\ntime = 933 ms",
+        xytext=(20, -55), textcoords="offset points", arrowprops=arrowprops(90))
 
-    plt.savefig(OUTPUT_TXACT_NAME, bbox_inches="tight")
+    plt.savefig(OUTPUT_NAME, bbox_inches="tight")
     #plt.show()
 
 (quartiles_original, quartiles_txact) = parse_file(FILE)
 speedup_base = quartiles_original[(1, 1)]["median"]
-
 speedups_original = calculate_speedups(quartiles_original, speedup_base, "original")
 errors_original = calculate_errors(speedups_original)
-draw_speedup_original(speedups_original, errors_original)
-
 speedups_txact = calculate_speedups(quartiles_txact, speedup_base, "txact")
 errors_txact = calculate_errors(speedups_txact)
-draw_speedup_txact(speedups_txact, errors_txact)
+draw_speedup(speedups_original, errors_original, speedups_txact, errors_txact)
